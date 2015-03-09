81f0c7d389c5fc95fda11dc48a603af2e92a1da5
Kevin Martensson <kevinmartensson@gmail.com>
2015-03-09 18:48:05 +0100

    Tweaks

--- index.js
+++ index.js
@@ -4,23 +4,16 @@ var sounds = require('./sounds.json');
 var Player = require('player');
 var unique = require('unique-random');
 
-/**
- * Play animal sounds
- *
- * @param {String} animal
- * @api public
- */
-
 module.exports = function (animal) {
 	var sound = unique(0, sounds.length - 1)();
 
 	if (animal) {
 		sounds = sounds.filter(function (el) {
-			return new RegExp(animal, 'gi').test(el);
+			return new RegExp(animal, 'i').test(el);
 		});
 
 		sound = unique(0, sounds.length - 1)();
 	}
 
-	new Player(sounds[sound], { stream: true }).play();
+	new Player(sounds[sound], {stream: true}).play();
 };
