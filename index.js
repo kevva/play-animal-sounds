b61f08e6fc801fd09972d0a319cbc0caf5966815
Kevin Martensson <kevinmartensson@gmail.com>
2016-09-06 14:41:04 +0200

    Add offline support
    Fixes #3.

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 'use strict';
+const path = require('path');
+const Conf = require('conf');
+const download = require('download');
+const filenamify = require('filenamify');
+const isOnline = require('is-online');
+const pify = require('pify');
 const Player = require('player');
 const randomItem = require('random-item');
+const xdgBasedir = require('xdg-basedir');
 const sounds = require('./sounds.json');
 
-module.exports = animal => {
-	let sound = randomItem(sounds);
+const playSound = sound => new Promise((resolve, reject) => {
+	const player = new Player(sound);
 
-	if (animal) {
-		sound = randomItem(sounds.filter(x => new RegExp(animal, 'i').test(x)));
+	player.on('error', err => {
+		if (err.includes('No next song was found')) {
+			resolve(sound);
+		}
+
+		reject(err);
+	});
+
+	player.on('end', () => resolve(sound));
+	player.play();
+});
+
+module.exports = animal => {
+	const config = new Conf();
+	const sound = randomItem(animal ? sounds.filter(x => new RegExp(animal, 'i').test(x)) : sounds);
+	const name = filenamify(path.basename(sound));
+	const dest = path.join(xdgBasedir.data, name);
+	const cached = config.get(name);
+
+	if (cached) {
+		return playSound(cached);
 	}
 
-	return new Promise((resolve, reject) => {
-		const player = new Player(sound);
+	return pify(isOnline)().then(online => {
+		if (!online) {
+			throw new Error('Couldn\'t play sound, check your internet connection');
+		}
 
-		player.on('error', err => {
-			if (err.includes('No next song was found')) {
-				resolve(sound);
-			}
-
-			reject(err);
+		return download(sound, xdgBasedir.data).then(() => {
+			config.set(name, dest);
+			return playSound(dest);
 		});
-
-		player.on('end', () => resolve(sound));
-		player.play();
 	});
 };
